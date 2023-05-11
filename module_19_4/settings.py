6ae4c28325817b48c92fc9292a8f05a6407c6847
topkek777 <104274635+topkek777@users.noreply.github.com>
2023-05-11 22:29:04 +0300

    Add files via upload

---
+++ module_19_4/settings.py
@@ -0,0 +1,11 @@
+# import os
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# valid_email = os.getenv("valid_email")
+# valid_password = os.getenv("valid_password")
+
+valid_email = "mireje6603@appxapi.com"
+valid_password = "12345kek"
